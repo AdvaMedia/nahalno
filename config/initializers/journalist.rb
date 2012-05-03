a2032964287910399caab52042cc635f72e20d5f
Kirillov Alexander <saratovsource@gmail.com>
2012-05-04 03:57:09 +0400

    update translations

--- config/initializers/journalist.rb
+++ config/initializers/journalist.rb
@@ -2,7 +2,7 @@ Journalist.configure do |config|
   config.multi_sites = false
   config.hosting = :auto
   config.delayed_job = false
-  config.default_locale = :en
+  config.default_locale = :ru
   config.enable_logs = true
   config.mailer_sender = 'support'
 end
